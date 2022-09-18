71bae1546b8caeebc5a35177c0a538aaaabd25b8
Kyle Miller <millerkyle85@gmail.com>
2022-09-18 12:38:29 -0700

    import AnimalShelter file

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import AnimalShelter from './AnimalShelter.js';
+
+function App() {
+  return (
+    <React.Fragment>
+      < AnimalShelter />
+    </React.Fragment>
+  );
+}
+
+export default App;
